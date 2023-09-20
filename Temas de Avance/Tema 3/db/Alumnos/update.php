b599d4ebc50ef9f677383514089731755038e863
GONZALES <gonzalesfranz2019@gmail.com>
2023-09-20 18:19:50 -0400

    Update code

--- Temas de Avance/Tema 3/db/Alumnos/update.php
+++ Temas de Avance/Tema 3/db/Alumnos/update.php
@@ -1,25 +1,42 @@
 <?php
-    include('verificar.php');
-    include('permisos.php');
-    include('../db.php');
+include('verificar.php');
+include('permisos.php');
+include('../db.php');
 
-    if(isset($_POST['id'])){
-        $id = $_POST['id'];
-        $nombre = $_POST['nombre'];
-        $apellido = $_POST['apellido'];
-        $CU = $_POST['CU'];
-        $id_carrera = $_POST['id_carrera'];
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    // $fotografia = $_POST['fotografia']['name'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $CU = $_POST['CU'];
+    $id_carrera = $_POST['id_carrera'];
 
+    
+    // Para actualizar la imagen
+    if (isset($_FILES['fotografia'])) {
+        $archivo_original = (isset($_FILES['fotografia']['name'])) ? $_FILES['fotografia']['name'] : '';
+        $arreglo_img = explode(".", $archivo_original);
+        $extension = $arreglo_img[1];
+        $fotografia = uniqid() . '.' . $extension;
+        move_uploaded_file($_FILES['fotografia']['tmp_name'], './images/' . $fotografia);
+        // copy($_FILES['fotografia']['tmp_name'], './images/' . $fotografia);
+
+        $sql = "UPDATE alumnos SET fotografia = '$fotografia', nombre = '$nombre', apellido = '$apellido', CU = '$CU', id_carrera = $id_carrera WHERE id = $id";
+    }else{
         $sql = "UPDATE alumnos SET nombre = '$nombre', apellido = '$apellido', CU = '$CU', id_carrera = $id_carrera WHERE id = $id";
-
-        // echo $sql;
-        if($connect->query($sql) === TRUE){
-            echo "Se actualizó correctamente";
-        }else{
-            echo "Error: " . $sql . "<br>" . $connect->error;
-        }
-        $connect->close();
     }
+    
+
+    // $sql = "UPDATE alumnos SET nombre = '$nombre', apellido = '$apellido', CU = '$CU', id_carrera = $id_carrera WHERE id = $id";
+
+    // echo $sql;
+    if ($connect->query($sql) === TRUE) {
+        echo "Se actualizó correctamente";
+    } else {
+        echo "Error: " . $sql . "<br>" . $connect->error;
+    }
+    $connect->close();
+}
 
 ?>
 
