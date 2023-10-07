e07f1f4adece1d13168413c1a11d768c1e1a8016
GONZALES <gonzalesfranz2019@gmail.com>
2023-10-07 17:55:58 -0400

    Tema 4 Javascript

---
+++ Temas de Avance/Tema 4/Js/Arreglos/arreglos.js
@@ -0,0 +1,20 @@
+var dias = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+  ];
+
+//   for (i in dias) {
+//     console.log(dias[i]);
+//   }
+
+  var string = "Hola mundo";
+  // sacar un elemento de la posicion
+  console.log('la letra en su posicio i es: ' + string.charAt(5)); 
+
+  // concatenar una cadena, unir cadena
+  console.log(string.concat(' ', 'natural'))
